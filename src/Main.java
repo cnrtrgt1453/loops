15fcb6393dec4ba7c042e389dfe67f0fedcd275e
cnrtrgt1453 <canerturgut34@gmail.com>
2019-09-29 13:23:15 +0300

    For Döngüsünde yaptığımız herşey, while döngüsü ile de yapılır..

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //For Döngüsü
         for (int i=1;i<=10;i++)
             System.out.println(i);
 
-        System.out.println("Döngü sona erdi..");
+        System.out.println("For döngü sona erdi..");
+
+        //While
+        /*while döngüsünün içerisine girmeden önce
+         i yi dışarıda tanımlamamız gerekir*/
+        int i=1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("while döngüsü bitti");
     }
 }
